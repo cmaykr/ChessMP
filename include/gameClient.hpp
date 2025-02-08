0c2eddd1e6f57efb72d8f7976155aadb16c6c31d
cmaykr <dj@netatonce.net>
2025-02-08 18:28:28 +0100

    Adds local isMoveValid and isPieceBlockingTarget method to client

--- include/gameClient.hpp
+++ include/gameClient.hpp
@@ -34,6 +34,9 @@ private:
     /// @return True if the move is valid, false otherwise.
     bool tryMove(int chosenX, int chosenY, int targetX, int targetY);
 
+    bool isMoveValid(int startX, int startY, int targetX, int targetY, std::array<std::array<Piece, 8>, 8> localBoard);
+    bool isPieceBlockingTarget(int startX, int startY, int targetX, int targetY, std::array<std::array<Piece, 8>, 8> localBoard);
+
     std::array<std::array<Piece, 8>, 8> localBoard;
     bool isPlayerWhitesTurn{true};
     bool clientIsPlayerWhite;
